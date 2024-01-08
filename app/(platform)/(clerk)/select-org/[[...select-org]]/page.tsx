ca4cda3f7d18843aea76f6698c65e18caf644017
Vankhiem <vankhiem@gmail.com>
2024-01-08 10:13:02 +0700

    organizations

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,8 @@
+import { OrganizationList } from "@clerk/nextjs";
+import React from "react";
+
+function CreateOrganizationPage() {
+  return <div>CreateOrganizationPage</div>;
+}
+
+export default CreateOrganizationPage;
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,8 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import React from "react";
+
+function OrganiztionPage() {
+  return <div></div>;
+}
+
+export default OrganiztionPage;
